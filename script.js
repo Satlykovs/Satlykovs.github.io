84cb74725a556ff0df5d752f5c09ec154ae4b96b
Satlykovs <satlykovs@gmail.com>
2024-10-17 21:18:28 +0300

    chore: fixing bugs

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Инициализация fullPage.js
+
 new fullpage('#fullpage', {
     autoScrolling: true,
     scrollHorizontally: true
 });
 
-// Обработка клика по кнопке меню
+
 document.getElementById('menu-button').addEventListener('click', function() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('active');

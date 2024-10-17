77817bca2b0eac1c6252869e71b4730158de7415
Satlykovs <satlykovs@gmail.com>
2024-10-17 21:12:02 +0300

    chore: refactoring

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-const menu = document.getElementById('menu');
-const menuButton = document.getElementById('menu-button');
-
-
-
-document.addEventListener('mousemove', (e) => {
-    if (e.clientX < 200) {
-        menu.style.left = '0';
-    } else {
-        if (menu.style.left === '0px') {
-            menu.style.left = '-200px';
-        }
-    }
+// Инициализация fullPage.js
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    scrollHorizontally: true
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    var loader = document.getElementById("loader");
-
-    
-    setTimeout(function() {
-        loader.style.display = "none";
-    }, 500);
+// Обработка клика по кнопке меню
+document.getElementById('menu-button').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active');
 });
+
